ad4407917e979e17317cb10f5b45a4fbaae25c7b
Juan Diego <jdiegopm12@gmail.com>
2024-02-26 18:51:17 -0500

    Add session 60 code

---
+++ 02-car-dealership/src/brands/brands.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuid} from 'uuid';
+
+@Injectable()
+export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    }
+  ];
+
+  create(createBrandDto: CreateBrandDto) {
+    const brand: Brand = {
+      id: uuid(),
+      createdAt: new Date().getTime(),
+      ...createBrandDto,
+    };
+    this.brands.push(brand)
+    return brand;
+  }
+
+  findAll() {
+    return `This action returns all brands`;
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find(brand => brand.id === id);
+    if(!brand) 
+      throw new NotFoundException(`Branch with ${id} not found`);
+
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDb = this.findOne(id);
+    this.brands = this.brands.map((brand) => {
+      if (brand.id === id) {
+        brandDb = Object.assign(brand, updateBrandDto, {updatedAt: new Date().getTime})
+        return brandDb
+      } else {
+        return brand;
+      }
+    })
+    return brandDb;
+  }
+
+  remove(id: string) {
+    this.brands = this.brands.filter(brand => brand.id !== id)
+  }
+}
---
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interface/car.interface';
+import {v4 as uuid} from 'uuid';
+import { CreateCarDto, UpdateCarDto } from './dtos';
+
+@Injectable()
+export class CarsService {
+  private cars: Car[] = [
+    {
+      id: uuid(),
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: uuid(),
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: uuid(),
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
+
+  findAll(): Car[] {
+    return this.cars
+  }
+
+  findOneById(id: string): Car {
+    const car = this.cars.find(c => c.id === id)
+    if (!car)
+      throw new NotFoundException(`Car with id "${id}" not found`)
+
+    return car
+  }
+
+  create(createCarDto: CreateCarDto): Car {
+    const newCar: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+
+    this.cars.push(newCar)
+    return newCar;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto): Car {
+    let carDb = this.findOneById(id);
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDb = Object.assign(car, updateCarDto, {id}) as Car
+        return carDb
+      } else {
+        return car
+      }
+    })
+    return carDb;
+  }
+
+  delete(id: string): Car {
+    const carDb = this.findOneById(id);
+    this.cars = this.cars.filter(c => c.id !== id)
+    return carDb;
+  }
+}
